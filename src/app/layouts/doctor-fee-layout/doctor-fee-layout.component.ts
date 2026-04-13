9486e8bd1459afb0ac5c97a7e1bcab7f0a269f99
KamrujjamanRony <kamrujjaman0228@gmail.com>
2026-04-13 11:11:55 +0600

    refactor: update DoctorFeeLayoutComponent with improved layout and user information display

--- src/app/layouts/doctor-fee-layout/doctor-fee-layout.component.ts
+++ src/app/layouts/doctor-fee-layout/doctor-fee-layout.component.ts
@@ -1,28 +1,32 @@
 import { Component, inject, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { DoctorFeeSidebarComponent } from "../../doctor-fee/components/doctor-fee-sidebar/doctor-fee-sidebar.component";
 import { AuthService } from '../../settings/services/auth.service';
+import { environment } from '../../../environments/environment';
+import { ThemeToggle } from "../../utils/theme-toggle/theme-toggle";
+import { Breadcrumb } from "../../utils/breadcrumb/breadcrumb";
 
 @Component({
   selector: 'app-doctor-fee-layout',
-  imports: [RouterOutlet, DoctorFeeSidebarComponent],
+  imports: [RouterOutlet, RouterLink, DoctorFeeSidebarComponent, ThemeToggle, Breadcrumb],
   templateUrl: './doctor-fee-layout.component.html',
   styleUrl: './doctor-fee-layout.component.css'
 })
 export class DoctorFeeLayoutComponent {
   private authService = inject(AuthService);
   user = signal<any>({});
+  currentDate = new Date().toLocaleDateString('en-GB');
+  companyName = environment.companyName;
 
   ngOnInit() {
     this.user.set(this.authService.getUser());
   }
 
   isMenuPermission(menuName: string) {
-    const menuPermission = this.user()?.userMenu.find((user: any) => user?.menuName.toLowerCase() === menuName.toLowerCase());
+    const menuPermission = this.user()?.userMenu?.find((user: any) => user?.menuName?.toLowerCase() === menuName?.toLowerCase());
     if (menuPermission) {
       return true;
     }
     return false;
   }
-
 }
