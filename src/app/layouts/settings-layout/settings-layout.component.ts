4aa7dbeaf3b2d493e577e2a562086cdda03ff3a9
KamrujjamanRony <kamrujjaman0228@gmail.com>
2026-04-13 12:14:00 +0600

    refactor: enhance SettingsLayoutComponent with improved header and user information display

--- src/app/layouts/settings-layout/settings-layout.component.ts
+++ src/app/layouts/settings-layout/settings-layout.component.ts
@@ -1,17 +1,22 @@
 import { Component, inject, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { SettingsSidebarComponent } from "../../settings/components/settings-sidebar/settings-sidebar.component";
 import { AuthService } from '../../settings/services/auth.service';
+import { environment } from '../../../environments/environment';
+import { ThemeToggle } from "../../utils/theme-toggle/theme-toggle";
+import { Breadcrumb } from "../../utils/breadcrumb/breadcrumb";
 
 @Component({
   selector: 'app-settings-layout',
-  imports: [RouterOutlet, SettingsSidebarComponent],
+  imports: [RouterOutlet, RouterLink, SettingsSidebarComponent, ThemeToggle, Breadcrumb],
   templateUrl: './settings-layout.component.html',
   styleUrl: './settings-layout.component.css'
 })
 export class SettingsLayoutComponent {
   private authService = inject(AuthService);
   user = signal<any>({});
+  currentDate = new Date().toLocaleDateString('en-GB');
+  companyName = environment.companyName;
 
   ngOnInit() {
     this.user.set(this.authService.getUser());
